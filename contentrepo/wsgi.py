f70f7c3af0e7c76828f709686cda432b93e78eab
Codie Roelf <codiebeulaine@gmail.com>
2021-03-11 17:04:59 +0200

    change settings

--- contentrepo/wsgi.py
+++ contentrepo/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "contentrepo.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "contentrepo.settings.production")
 
 application = get_wsgi_application()
