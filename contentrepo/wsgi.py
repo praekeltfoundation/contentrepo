e0e538aa1dc730c0251bcee343f2d789027b434f
Codie Roelf <codiebeulaine@gmail.com>
2021-03-18 08:27:51 +0200

    update settings for prod

--- contentrepo/wsgi.py
+++ contentrepo/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "contentrepo.settings.production")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "contentrepo.settings.production")
 
 application = get_wsgi_application()
