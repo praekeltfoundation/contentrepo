9c195ea7b08ddf7385742458fe79e7fb1136fe5b
Codie Roelf <codiebeulaine@gmail.com>
2021-04-15 17:08:57 +0200

    remove commented out settings

--- contentrepo/settings/dev.py
+++ contentrepo/settings/dev.py
@@ -12,7 +12,7 @@ ALLOWED_HOSTS = ['*']
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
-# try:
-#     from .local import *
-# except ImportError:
-#     pass
+try:
+    from .local import *
+except ImportError:
+    pass
