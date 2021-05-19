b0e99c7e8b547689e5562900b359ead0a07a88be
Codie Roelf <codiebeulaine@gmail.com>
2021-05-19 19:46:14 +0200

    lint

--- home/management/commands/import_csv_content.py
+++ home/management/commands/import_csv_content.py
@@ -1,5 +1,5 @@
 import csv
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from home.models import ContentPage, HomePage
 from wagtail.core.rich_text import RichText
 from taggit.models import Tag
