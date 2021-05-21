16f8910e977befd687cf2eb2d60aa9e78513b97b
Codie Roelf <codiebeulaine@gmail.com>
2021-05-21 11:10:36 +0200

    use database url for prod

--- contentrepo/settings/production.py
+++ contentrepo/settings/production.py
@@ -1,19 +1,24 @@
 from .base import *  # noqa
+import dj_database_url
 
 DEBUG = False
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ.get('DATABASE_NAME', 'name'),
+#         'USER': os.environ.get('DATABASE_LOGIN', 'user'),
+#         'PASSWORD': os.environ.get(
+#             'DATABASE_PASSWORD', 'password'),
+#         'HOST': os.environ.get('DATABASE_HOST', ''),
+#         'PORT': '5432',
+#         'CONN_MAX_AGE': 600,
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DATABASE_NAME', 'name'),
-        'USER': os.environ.get('DATABASE_LOGIN', 'user'),
-        'PASSWORD': os.environ.get(
-            'DATABASE_PASSWORD', 'password'),
-        'HOST': os.environ.get('DATABASE_HOST', ''),
-        'PORT': '5432',
-        'CONN_MAX_AGE': 600,
-    }
-}
+    'default': dj_database_url.config(
+        default='sqlite:///%s' % (join(PROJECT_ROOT, 'contentrepo.db'),))}
 
 SECRET_KEY = os.environ.get(
     'SECRET_KEY', '')
