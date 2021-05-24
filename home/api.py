31f801d78c3be8b6fbe4df35c84e4dac20017516
Codie Roelf <codiebeulaine@gmail.com>
2021-05-24 14:17:45 +0200

    lint

--- home/api.py
+++ home/api.py
@@ -3,7 +3,6 @@ from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.images.api.v2.views import ImagesAPIViewSet
 from wagtail.documents.api.v2.views import DocumentsAPIViewSet
 from .serializers import ContentPageSerializer
-from django.views.decorators.cache import cache_page
 from.models import ContentPageTag
 
 
