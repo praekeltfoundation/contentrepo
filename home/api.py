ee4403a860f8a5a20f418a0bed1b09418d3ab58c
Codie Roelf <codiebeulaine@gmail.com>
2021-05-26 13:14:38 +0200

    lint

--- home/api.py
+++ home/api.py
@@ -1,10 +1,10 @@
+from rest_framework.pagination import PageNumberPagination
 from wagtail.api.v2.views import PagesAPIViewSet
 from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.images.api.v2.views import ImagesAPIViewSet
 from wagtail.documents.api.v2.views import DocumentsAPIViewSet
 from .serializers import ContentPageSerializer
 from.models import ContentPageTag
-from rest_framework.pagination import PageNumberPagination
 
 
 class ContentPagesViewSet(PagesAPIViewSet):
