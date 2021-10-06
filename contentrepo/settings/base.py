dda11fcf81b1527813aaef886bbf9369ea0210a9
Codie Roelf <codiebeulaine@gmail.com>
2021-10-06 15:28:43 +0200

    black

--- contentrepo/settings/base.py
+++ contentrepo/settings/base.py
@@ -181,7 +181,7 @@ CACHES = {
         "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+        },
     }
 }
 
