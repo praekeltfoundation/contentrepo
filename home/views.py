072c6328aba343843755300c093598026ec2b99a
Codie Roelf <codiebeulaine@gmail.com>
2021-10-13 08:09:07 +0200

    add improt button fucntionality

---
+++ home/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .forms import UploadFileForm
+from .utils import import_content
+
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            job = handle_uploaded_file(request.FILES['file'])
+            if job == "success":
+                return HttpResponseRedirect('/admin/')
+            else:
+                form.add_error('file', 'Unsuccessful.')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
+
+
+def handle_uploaded_file(f):
+    import_content(f)
