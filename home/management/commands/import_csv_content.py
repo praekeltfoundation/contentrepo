89e182f591933e290f3f5cdca79ebc3c00f36fa4
megh-k <meghan@praekelt.org>
2022-05-24 13:50:32 +0200

    add csv import support for messenger and viber

--- home/management/commands/import_csv_content.py
+++ home/management/commands/import_csv_content.py
@@ -24,7 +24,7 @@ class Command(BaseCommand):
                     body = body + [("paragraph", RichText(line))]
             return body
 
-        def get_text_body(raw):
+        def get_text_body(raw, type_of_message):
             if options["splitmessages"] == "yes":
                 struct_blocks = []
                 if options["newline"]:
@@ -39,7 +39,7 @@ class Command(BaseCommand):
                             ]
                         )
                         block_value = block.to_python({"message": row})
-                        struct_blocks.append(("Whatsapp_Message", block_value))
+                        struct_blocks.append((type_of_message, block_value))
                 return struct_blocks
             else:
                 if raw:
@@ -49,7 +49,7 @@ class Command(BaseCommand):
                         ]
                     )
                     block_value = block.to_python({"message": raw})
-                    return [("Whatsapp_Message", block_value)]
+                    return [(type_of_message, block_value)]
 
         def create_tags(row, page):
             tags = row["tags"].split(",")
@@ -73,9 +73,80 @@ class Command(BaseCommand):
                 "whatsapp_body",
                 "parent",
             ):
-                if row[field]:
+                if field in row and row[field]:
                     row[field] = str(row[field]).strip()
             return row
+
+        def add_web(row):
+            # if 'web_title' not in row.keys():
+            if "web_title" not in row.keys() or not row["web_title"]:
+                return
+            page = ContentPage(
+                title=row["web_title"],
+                subtitle=row["web_subtitle"],
+                body=get_rich_text_body(row["web_body"]),
+                enable_web=True,
+            )
+            return page
+
+        def add_whatsapp(row, page=None):
+            if "whatsapp_title" not in row.keys() or not row["whatsapp_title"]:
+                return page
+
+            if not page:
+                return ContentPage(
+                    title=row["whatsapp_title"],
+                    enable_whatsapp=True,
+                    whatsapp_title=row["whatsapp_title"],
+                    whatsapp_body=get_text_body(
+                        row["whatsapp_body"], "Whatsapp_Message"
+                    ),
+                )
+            else:
+                page.enable_whatsapp = True
+                page.whatsapp_title = row["whatsapp_title"]
+                page.whatsapp_body = get_text_body(
+                    row["whatsapp_body"], "Whatsapp_Message"
+                )
+                return page
+
+        def add_messenger(row, page=None):
+            if "messenger_title" not in row.keys() or not row["messenger_title"]:
+                return page
+
+            if not page:
+                return ContentPage(
+                    title=row["messenger_title"],
+                    enable_messenger=True,
+                    messenger_title=row["messenger_title"],
+                    messenger_body=get_text_body(
+                        row["messenger_body"], "messenger_block"
+                    ),
+                )
+            else:
+                page.enable_messenger = True
+                page.messenger_title = row["messenger_title"]
+                page.messenger_body = get_text_body(
+                    row["messenger_body"], "messenger_block"
+                )
+                return page
+
+        def add_viber(row, page=None):
+            if "viber_title" not in row.keys() or not row["viber_title"]:
+                return page
+
+            if not page:
+                return ContentPage(
+                    title=row["viber_title"],
+                    enable_viberr=True,
+                    viber_title=row["viber_title"],
+                    viber_body=get_text_body(row["viber_body"], "viber_message"),
+                )
+            else:
+                page.enable_viber = True
+                page.viber_title = row["viber_title"]
+                page.viber_body = get_text_body(row["viber_body"], "viber_message")
+                return page
 
         if options["purge"] == "yes":
             ContentPage.objects.all().delete()
@@ -86,16 +157,14 @@ class Command(BaseCommand):
             reader = csv.DictReader(f)
             for row in reader:
                 row = clean_row(row)
-                contentpage = ContentPage(
-                    title=row["web_title"],
-                    subtitle=row["web_subtitle"],
-                    body=get_rich_text_body(row["web_body"]),
-                    enable_whatsapp=True,
-                    whatsapp_title=row["whatsapp_title"],
-                    whatsapp_body=get_text_body(row["whatsapp_body"]),
-                )
-                create_tags(row, contentpage)
-                add_parent(row, contentpage, home_page)
-                contentpage.save_revision().publish()
+                contentpage = add_web(row)
+                contentpage = add_whatsapp(row, contentpage)
+                contentpage = add_messenger(row, contentpage)
+                contentpage = add_viber(row, contentpage)
+
+                if contentpage:
+                    create_tags(row, contentpage)
+                    add_parent(row, contentpage, home_page)
+                    # contentpage.save_revision().publish()
 
             self.stdout.write(self.style.SUCCESS("Successfully imported Content Pages"))
