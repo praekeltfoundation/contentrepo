af3f7ccb80413713fa68e688742c747c398ba10e
megh-k <meghan@praekelt.org>
2022-09-08 17:25:28 +0200

    Move BoundPanel html into template as required by Wagtail 4.0.1

--- home/panels.py
+++ home/panels.py
@@ -4,6 +4,14 @@ from wagtail.admin.panels import Panel
 
 class PageRatingPanel(Panel):
     class BoundPanel(Panel.BoundPanel):
+        template_name = 'panels/page_rating_panel.html'
+
+        def get_context_data(self, parent_context=None):
+            context = super().get_context_data(parent_context)
+            context["page_value"] = self.instance.page_rating
+            context["revision_value"] = self.instance.latest_revision_rating
+            return context
+
         def render(self):
             page_value = self.instance.page_rating
             revision_value = self.instance.latest_revision_rating
