236896c763b4f0c7762576da56b526c28d645bed
megh-k <meghan@praekelt.org>
2022-09-12 10:55:17 +0200

    Remove render and render_as_object from custom panel

--- home/panels.py
+++ home/panels.py
@@ -1,4 +1,3 @@
-from django.utils.html import format_html
 from wagtail.admin.panels import Panel
 
 
@@ -11,23 +10,3 @@ class PageRatingPanel(Panel):
             context["page_value"] = self.instance.page_rating
             context["revision_value"] = self.instance.latest_revision_rating
             return context
-
-        def render(self):
-            page_value = self.instance.page_rating
-            revision_value = self.instance.latest_revision_rating
-            return format_html(
-                '<ul class="fields"><li><div style="padding-top: 1.2em;">'
-                'Page: {}</div><div style="padding-top: 1.2em;">'
-                "Latest Revision: {}</div></li></ul>",
-                page_value,
-                revision_value,
-            )
-
-        def render_as_object(self):
-            return format_html(
-                "<fieldset><legend>{}</legend>"
-                '<ul class="fields"><li><div class="field">{}</div></li></ul>'
-                "</fieldset>",
-                self.heading,
-                self.render(),
-            )
