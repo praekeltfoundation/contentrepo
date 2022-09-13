99e880fa1d3e1b63b3e32d02afdfcc6583ff443d
megh-k <meghan@praekelt.org>
2022-09-13 14:27:43 +0200

    Fix page ratings such that a new page can be created

--- home/panels.py
+++ home/panels.py
@@ -5,9 +5,12 @@ class PageRatingPanel(Panel):
     class BoundPanel(Panel.BoundPanel):
         template_name = "panels/page_rating_panel.html"
 
-        # def get_context_data(self, parent_context=None):
-        #     context = super().get_context_data(parent_context)
-        #     if self.instance:
-        #         context["page_value"] = self.instance.page_rating
-        #         context["revision_value"] = self.instance.latest_revision_rating
-        #     return context
+        def get_context_data(self, parent_context=None):
+            context = super().get_context_data(parent_context)
+            if self.instance.id:
+                context["page_value"] = self.instance.page_rating
+                context["revision_value"] = self.instance.latest_revision_rating
+            else:
+                context["page_value"] = "(No ratings yet)"
+                context["revision_value"] = "(No revisions yet)"
+            return context
