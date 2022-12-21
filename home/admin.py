7b41d1cc8791aff4f5caeb383247480fb90a304a
Kaitlyn <kaitlyn@praekelt.org>
2022-12-21 13:20:50 +0200

    Linting fixes

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+
 from .models import PageView
+
 
 @admin.register(PageView)
 class PageViewAdmin(admin.ModelAdmin):
-  list_display = [field.name for field in PageView._meta.get_fields()]
+    list_display = [field.name for field in PageView._meta.get_fields()]
