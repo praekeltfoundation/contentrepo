aee8c1d273a19b6f8f80759feb716e91ac7ff2f3
Buhle <buhle@praekelt.org>
2023-06-27 14:29:13 +0200

    Sort imports

--- home/api.py
+++ home/api.py
@@ -1,12 +1,12 @@
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
+from rest_framework.exceptions import ValidationError
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import PageNumberPagination
 from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.api.v2.views import BaseAPIViewSet, PagesAPIViewSet
 from wagtail.documents.api.v2.views import DocumentsAPIViewSet
 from wagtail.images.api.v2.views import ImagesAPIViewSet
-from rest_framework.exceptions import ValidationError
 
 from .models import OrderedContentSet
 from .serializers import ContentPageSerializer, OrderedContentSetSerializer
