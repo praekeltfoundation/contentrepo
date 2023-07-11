ac6ea5ee173fec3078cf4645f441c6fdad37f447
Kaitlyn <kaitlyn@praekelt.org>
2023-07-11 06:56:40 +0200

    Improve embedding pre-processing and move it to utils file

--- home/models.py
+++ home/models.py
@@ -22,6 +22,7 @@ from wagtail_content_import.models import ContentImportMixin
 from wagtailmedia.blocks import AbstractMediaChooserBlock
 
 from .panels import PageRatingPanel
+from .utils import preprocess_content_for_embedding
 from .whatsapp import create_whatsapp_template
 
 from .constants import (  # isort:skip
@@ -578,24 +579,11 @@ class ContentPage(Page, ContentImportMixin):
 
 @receiver(pre_save, sender=ContentPage)
 def update_embedding(sender, instance, *args, **kwargs):
-    emoji_pattern = re.compile(
-        "["
-        "\U0001F600-\U0001F64F"
-        "\U0001F300-\U0001F5FF"
-        "\U0001F680-\U0001F6FF"
-        "\U0001F1E0-\U0001F1FF"
-        "\U00002702-\U000027B0"
-        "\U000024C2-\U0001F251"
-        "\U0001FA00-\U0001FAFF"
-        "\U0001F900-\U0001F9FF"
-        "]+",
-        flags=re.UNICODE,
-    )
-    body = emoji_pattern.sub(r"", instance.body[0].value.source)
+    body = preprocess_content_for_embedding(instance.body[0].value.source)
 
     instance.embedding = {
         "values": [
-            float(i) for i in model.encode(body.replace("\n\n", " ").replace("*", ""))
+            float(i) for i in model.encode(body)
         ]
     }
 
--- home/utils.py
+++ home/utils.py
@@ -1,11 +1,13 @@
 import copy
 import csv
 import io
+import re
 from dataclasses import dataclass
 from io import BytesIO
 from json import dumps
 from math import ceil
 from typing import List, Tuple, Union
+from urllib.parse import unquote
 
 import numpy as np
 from django.http import HttpResponse
@@ -1202,3 +1204,53 @@ class ContentSheetRow:
         if next_prompt:
             return next_prompt
         return ""
+
+
+def _generate_regex_dashed_url(n_min_dashed_words_url):
+    """
+    Generate regex to parse dashed words from URL
+
+    Parameters
+    ----------
+    n_min_dashed_words_url : int
+        The number of words that must be separated by dashes in a URL, to treat the
+        text as an actual relevant content summary
+
+        For example, many websites have "breaking-news" in the URL, but we
+        don't care to extract this as a content summary
+    """
+    word_and_dash = "[a-z0-9]+-"
+    regexp = (
+        r"\/(" + (n_min_dashed_words_url - 1) * word_and_dash + r"["
+        r"a-z0-9-]*)(?:\/|\.|$)"
+    )
+    return regexp
+
+
+def preprocess_content_for_embedding(content):
+    content = unquote(content) # Replace %xx escapes with their single-character equivalent.
+    urls = re.findall(r"((?:https?:\/\/|www)(?:[^ ]+))", content) # Find URLs
+    for url in urls: # Replace URL portions with n_words_min or more words that are separated by dashes
+        extract = re.findall(_generate_regex_dashed_url(2), url)
+        extract = " ".join(extract)
+        content = content.replace(url, extract)
+    content = "".join(content.split("*", 2)[2:]).replace('\n\n',' ').replace('\n',' ').replace('  ',' ').replace('*','') # Remove content piece title
+    if len(content)<2:
+        return content
+    if content[0]==' ': # Remove space trailing content title
+        content = content[1:]
+    emoji_pattern = re.compile("["
+    u"\U0001F600-\U0001F64F"  # emoticons
+    u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+    u"\U0001F680-\U0001F6FF"  # transport & map symbols
+    u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    u"\U00002500-\U00002BEF"  # Chinese characters
+    u"\U00002702-\U000027B0"  # Dingbats
+    u"\U000024C2-\U0001F251"
+    u"\U0001f926-\U0001f937"
+    u"\U00010000-\U0010ffff"
+    u"\u200d"
+    u"\u2640-\u2642"
+    "]+", flags=re.UNICODE)
+    content = emoji_pattern.sub(r'', content) # Remove emojis
+    return content
