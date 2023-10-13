407052f41ebdb25d1c83f6493550958abf54843c
Kaitlyn <kaitlyn@praekelt.org>
2023-10-13 14:12:51 +0200

    Add api endpoint for media

--- home/api.py
+++ home/api.py
@@ -8,6 +8,7 @@ from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.api.v2.views import BaseAPIViewSet, PagesAPIViewSet
 from wagtail.documents.api.v2.views import DocumentsAPIViewSet
 from wagtail.images.api.v2.views import ImagesAPIViewSet
+from wagtailmedia.api.views import MediaAPIViewSet
 
 from .models import OrderedContentSet
 from .serializers import ContentPageSerializer, OrderedContentSetSerializer
@@ -153,5 +154,6 @@ api_router.register_endpoint("pages", ContentPagesViewSet)
 api_router.register_endpoint("indexes", ContentPageIndexViewSet)
 api_router.register_endpoint("images", ImagesAPIViewSet)
 api_router.register_endpoint("documents", DocumentsAPIViewSet)
+api_router.register_endpoint("media", MediaAPIViewSet)
 
 api_router.register_endpoint("orderedcontent", OrderedContentSetViewSet)
