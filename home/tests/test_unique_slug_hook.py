892e43591525d8c0300fc71f36d86a2f38a97bf7
Rudi Giesler <rudi@reachdigitalhealth.org>
2023-10-31 17:12:45 +0200

    Add slug autofill

--- home/tests/test_unique_slug_hook.py
+++ home/tests/test_unique_slug_hook.py
@@ -1,7 +1,7 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
-from wagtail.models import Page
 
-from home.wagtail_hooks import create_unique_slug
+from home.models import ContentPage, HomePage
 
 
 class UniqueSlugHookTest(TestCase):
@@ -9,8 +9,28 @@ class UniqueSlugHookTest(TestCase):
         """
         If the slug exists, append a number so that it's a new slug
         """
-        home = Page.objects.first()
-        home.add_child(instance=Page(title="duplicate", slug="duplicate"))
-        home.add_child(instance=Page(title="duplicate", slug="duplicate-2"))
-        slug = create_unique_slug("duplicate")
+        home = HomePage.objects.first()
+        home.add_child(instance=ContentPage(title="duplicate", slug="duplicate"))
+        home.add_child(instance=ContentPage(title="duplicate", slug="duplicate-2"))
+        slug = ContentPage().get_unique_slug("duplicate")
         self.assertEqual(slug, "duplicate-3")
+
+    def test_duplicate_slug_on_creation(self):
+        """
+        If there's no slug specified, a unique one should be generated
+        """
+        home = HomePage.objects.first()
+        page1 = home.add_child(instance=ContentPage(title="duplicate"))
+        page2 = home.add_child(instance=ContentPage(title="duplicate"))
+        self.assertEqual(page1.slug, "duplicate")
+        self.assertEqual(page2.slug, "duplicate-2")
+
+    def test_duplicate_slug_error(self):
+        """
+        If a slug already exists, an error should be raised
+        """
+        home = HomePage.objects.first()
+        home.add_child(instance=ContentPage(title="duplicate", slug="duplicate"))
+        with self.assertRaises(ValidationError) as e:
+            home.add_child(instance=ContentPage(title="duplicate", slug="duplicate"))
+        self.assertIn("slug", e.exception.error_dict)
