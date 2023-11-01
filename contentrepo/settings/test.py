1bc7324a913a2e549b9e5a14872e6984449a9733
Rudi Giesler <rudi@reachdigitalhealth.org>
2023-11-01 16:12:11 +0200

    Use MD5 password hasher for tests

--- contentrepo/settings/test.py
+++ contentrepo/settings/test.py
@@ -2,3 +2,4 @@ from .dev import *  # noqa
 
 
 DATABASES = {"default": env.db("CONTENTREPO_DATABASE", default="sqlite://:memory:")}
+PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)
