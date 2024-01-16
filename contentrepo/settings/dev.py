f63990c0a25bc232e78cc79dd3f789087188b7ae
Mudiwa Matanda <mudiwa@Mudiwas-MacBook-Air.local>
2024-01-16 08:28:56 +0200

    variable-fix

--- contentrepo/settings/dev.py
+++ contentrepo/settings/dev.py
@@ -11,9 +11,7 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost"])
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-WHATSAPP_API_URL = "http://whatsapp"
-WHATSAPP_ACCESS_TOKEN = "fake-access-token"  # noqa: S105 (This is a test config.)
-FB_BUSINESS_ID = "27121231234"
+
 
 
 # We default to using local memory for dev and tests, so that Redis isn't a dependancy
--- contentrepo/settings/test.py
+++ contentrepo/settings/test.py
@@ -3,3 +3,7 @@ from .dev import *  # noqa
 
 DATABASES = {"default": env.db("CONTENTREPO_DATABASE", default="sqlite://:memory:")}
 PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)
+
+WHATSAPP_API_URL = "http://whatsapp"
+WHATSAPP_ACCESS_TOKEN = "fake-access-token"  # noqa: S105 (This is a test config.)
+FB_BUSINESS_ID = "27121231234"
