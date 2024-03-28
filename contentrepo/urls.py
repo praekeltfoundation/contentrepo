2d67588ea91f50b7efa5f36cf70dcdeb50e6c14c
Hlamalani <hlamalani@praekelt.org>
2024-03-28 09:33:48 +0200

    return status code 404 if page not found

--- contentrepo/urls.py
+++ contentrepo/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path, reverse_lazy
 from django.views.generic.base import RedirectView
