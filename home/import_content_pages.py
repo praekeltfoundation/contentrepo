3b226cfae3130ff4aeb653d85598e68d99b32ebe
Hlamalani <hlamalani@praekelt.org>
2024-04-22 13:06:50 +0200

    fix formationg

--- home/import_content_pages.py
+++ home/import_content_pages.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass, field, fields
 from datetime import datetime
 from io import BytesIO, StringIO
 from queue import Queue
-from typing import Any
+from typing import Any, Dict, Union, List
 from uuid import uuid4
 
 from django.core.exceptions import ObjectDoesNotExist, ValidationError  # type: ignore
@@ -497,7 +497,7 @@ class ShadowContentPage:
                     f"Validation error: {error_messsage}", self.row_num
                 )
 
-    def errors_to_strings(self, errs):
+    def errors_to_strings(self, elf, errs: Dict[str, Any]) -> Union[str, List[str]]):
         errors = errs[next(iter(errs))][0]
 
         if isinstance(errors, dict):
