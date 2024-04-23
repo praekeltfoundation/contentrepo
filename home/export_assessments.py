3a07eca6b88aa4bca6d91359691e3aca76e1bcf8
Rudi Giesler <rudi@reachdigitalhealth.org>
2024-04-23 15:21:12 +0200

    Refactor assessments export

--- home/export_assessments.py
+++ home/export_assessments.py
@@ -6,92 +6,75 @@ from dataclasses import asdict, astuple, dataclass, fields
 from math import ceil
 
 from django.http import HttpResponse  # type: ignore
-from openpyxl.styles import Border, Color, Font, NamedStyle, PatternFill, Side
+from openpyxl.styles import Border, Font, NamedStyle, Side
 from openpyxl.utils import get_column_letter
 from openpyxl.workbook import Workbook
 from openpyxl.worksheet.worksheet import Worksheet
-from wagtail.blocks import (  # type: ignore
-    StreamValue,
-    StructValue,  # type: ignore
-)
 from wagtail.query import PageQuerySet  # type: ignore
 
 
 @dataclass
 class ExportRow:
-    title: str = ""
-    tags: str = ""
-    slug: str = ""
-    locale: str = ""
-    high_result_page: str = ""
-    high_inflection: str = ""
-    medium_result_page: str = ""
-    medium_inflection: str = ""
-    low_result_page: str = ""
-    generic_error: str = ""
-    question: str = ""
-    error: str = ""
-    answers: str = ""
-    scores: str = ""
+    title: str
+    tags: str
+    slug: str
+    locale: str
+    high_result_page: str
+    high_inflection: str
+    medium_result_page: str
+    medium_inflection: str
+    low_result_page: str
+    generic_error: str
+    question: str
+    error: str
+    answers: str
+    scores: str
 
     @classmethod
     def headings(cls) -> list[str]:
         return [f.name for f in fields(cls)]
 
-    def to_dict(self) -> dict[str, str | int]:
+    def to_dict(self) -> dict[str, str]:
         return asdict(self)
 
-    def to_tuple(self) -> tuple[str | int, ...]:
+    def to_tuple(self) -> tuple[str]:
         return astuple(self)
 
 
 # Define your AssessmentExporter class
 class AssessmentExporter:
-    rows: list[dict[str | int, str | int]]
+    rows: list[ExportRow]
 
     def __init__(self, queryset: PageQuerySet):
         self.rows = []
         self.queryset = queryset
 
-    def perform_export(self) -> list[dict[str | int, str | int]]:
+    def perform_export(self) -> list[ExportRow]:
         for item in self.queryset:
-            for question_data in self.get_questions(item.questions):
+            for question in item.questions:
+                answers = [a["answer"] for a in question.value["answers"]]
+                scores = [a["score"] for a in question.value["answers"]]
                 self.rows.append(
-                    {
-                        "title": item.title,
-                        "tags": serialize_list(
+                    ExportRow(
+                        title=item.title,
+                        tags=serialize_list(
                             filter_non_empty(t.name for t in item.tags.all())
                         ),
-                        "slug": item.slug,
-                        "locale": str(item.locale.language_code),
-                        "high_result_page": str(item.high_result_page.slug),
-                        "high_inflection": item.high_inflection,
-                        "medium_result_page": str(item.medium_result_page.slug),
-                        "medium_inflection": item.medium_inflection,
-                        "low_result_page": str(item.low_result_page.slug),
-                        "generic_error": item.generic_error,
-                        **question_data,
-                    }
+                        slug=item.slug,
+                        locale=item.locale.language_code,
+                        high_result_page=item.high_result_page.slug,
+                        high_inflection=str(item.high_inflection),
+                        medium_result_page=item.medium_result_page.slug,
+                        medium_inflection=str(item.medium_inflection),
+                        low_result_page=item.low_result_page.slug,
+                        generic_error=item.generic_error,
+                        question=question.value["question"],
+                        error=question.value["error"],
+                        answers=serialize_list(answers),
+                        scores=serialize_list(scores),
+                    )
                 )
         return self.rows
-
-    def get_questions(self, questions: StreamValue) -> list[StructValue]:
-        question_data = []
-        for question in questions:
-            answers = []
-            scores = []
-            for answer in question.value["answers"]:
-                answers.append(answer["answer"])
-                scores.append(str(answer["score"]))
-            question_data.append(
-                {
-                    "question": (question.value["question"]),
-                    "error": (question.value["error"]),
-                    "answers": serialize_list(answers),
-                    "scores": serialize_list(scores),
-                }
-            )
-        return question_data
 
 
 def filter_non_empty(items: Iterable[str]) -> Iterator[str]:
@@ -111,124 +94,74 @@ def serialize_list(items: Iterable[str]) -> str:
 
 
 class AssessmentExportWriter:
-    rows: list[dict[str | int, str | int]]
+    rows: list[ExportRow]
 
-    def __init__(self, rows: list[dict[str | int, str | int]]):
+    def __init__(self, rows: list[ExportRow]):
         self.rows = rows
 
     def write_xlsx(self, response: HttpResponse) -> None:
         workbook = Workbook()
-        worksheet = workbook.active
-        worksheet.append(ExportRow.headings())  # type: ignore
+        worksheet: Worksheet = workbook.active  # type: ignore
+        worksheet.append(ExportRow.headings())
         for row in self.rows:
-            row_values = [
-                row["title"],
-                # row["page_id"],
-                row["tags"],
-                row["slug"],
-                row["locale"],
-                row["high_result_page"],
-                row["high_inflection"],
-                row["medium_result_page"],
-                row["medium_inflection"],
-                row["low_result_page"],
-                row["generic_error"],
-                row["question"],
-                row["error"],
-                row["answers"],
-                row["scores"],
-            ]
-            worksheet.append(row_values)  # type: ignore
-        _set_xlsx_styles(workbook, worksheet)  # type: ignore
+            worksheet.append(row.to_tuple())
+        _set_xlsx_styles(workbook, worksheet)
         workbook.save(response)
 
     def write_csv(self, response: HttpResponse) -> None:
-        csv_response = csv.writer(response)
-        csv_response.writerow(ExportRow.headings())
+        writer = csv.DictWriter(f=response, fieldnames=ExportRow.headings())
+        writer.writeheader()
 
-        # Write data rows
         for row in self.rows:
-            row_values = [
-                row["title"],
-                # row["page_id"],
-                row["tags"],
-                row["slug"],
-                row["locale"],
-                row["high_result_page"],
-                row["high_inflection"],
-                row["medium_result_page"],
-                row["medium_inflection"],
-                row["low_result_page"],
-                row["generic_error"],
-                row["question"],
-                row["error"],
-                row["answers"],
-                row["scores"],
-            ]
-            csv_response.writerow(row_values)
+            writer.writerow(row.to_dict())
 
 
 def _set_xlsx_styles(wb: Workbook, sheet: Worksheet) -> None:
     """Sets the style for the workbook adding any formatting that will make the sheet more aesthetically pleasing"""
     # Adjustment is because the size in openxlsx and google sheets are not equivalent
     adjustment = 7
-    # Padding
-    sheet.insert_cols(1)
 
     # Set columns based on best size
-
     column_widths_in_pts = {
         "title": 110,
-        # "page_id": 110,
         "tags": 110,
         "slug": 110,
-        "locale": 118,
+        "locale": 50,
         "high_result_page": 110,
         "high_inflection": 110,
-        "medium_result_page": 110,
+        "medium_result_page": 120,
         "medium_inflection": 110,
-        "low_result_page": 118,
-        "generic_error": 300,
+        "low_result_page": 110,
+        "generic_error": 370,
         "question": 370,
-        "error": 400,
-        "answer": 110,
+        "error": 370,
+        "answers": 370,
         "scores": 110,
     }
-
-    for index, column_width in enumerate(column_widths_in_pts.values(), 2):
-        sheet.column_dimensions[get_column_letter(index)].width = ceil(
-            column_width / adjustment
+    for column in sheet.iter_cols(max_row=1):
+        [cell] = column
+        width = column_widths_in_pts[str(cell.value)]
+        sheet.column_dimensions[get_column_letter(cell.col_idx)].width = ceil(
+            width / adjustment
         )
 
     # Freeze heading row and side panel, 1 added because it freezes before the column
     panel_column = get_column_letter(5)
     sheet.freeze_panes = sheet[f"{panel_column}2"]
 
-    # Colours
-    blue = Color(rgb="0099CCFF")
-
-    # Boarders
+    # Borders
     left_border = Border(left=Side(border_style="thin", color="FF000000"))
-
-    # Fills
-    blue_fill = PatternFill(patternType="solid", fgColor=blue)
 
     # Named Styles
     header_style = NamedStyle(name="header_style")
-    menu_style = NamedStyle(name="menu_style")
 
     # Set attributes to styles
     header_style.font = Font(bold=True, size=10)
-    menu_style.fill = blue_fill
-    menu_style.font = Font(bold=True, size=10)
 
     # Add named styles to wb
     wb.add_named_style(header_style)
-    wb.add_named_style(menu_style)
 
-    # column widths
-
-    # Set header style for row 1 and 2
+    # Set header style for row 1
     for row in sheet["1:2"]:
         for cell in row:
             cell.style = header_style
