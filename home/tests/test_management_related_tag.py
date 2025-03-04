78c19e0fd474e20a93a9929664c9c3ad9370dd63
devchima <developer.chima@gmail.com>
2025-03-04 04:43:42 -0500

    Added more tests

--- home/tests/test_management_related_tag.py
+++ home/tests/test_management_related_tag.py
@@ -86,3 +86,41 @@ class ManagementRelatedTag(TestCase):
         self.assertEqual(
             [p.value.id for p in page.related_pages], [self.related_page.id]
         )
+
+    def test_multiple_related_tags(self):
+        """
+        Check that the management command correctly handles multiple tags
+        prefixed with related_ for a single page.
+        """
+        additional_page = ContentPage(
+            title="Additional related page", slug="additional-related-page"
+        )
+        home_page = HomePage.objects.first()
+        home_page.add_child(instance=additional_page)
+        self.page.tags.add(Tag.objects.create(name=f"related_{additional_page.id}"))
+        self.page.save_revision().publish()
+
+        out = StringIO()
+        call_command("change_related_tag_to_related_page", "--no-dry-run", stdout=out)
+
+        self.assertIn(
+            f"Added related pages {{{self.related_page.id}, {additional_page.id}}} to {self.page}",
+            out.getvalue(),
+        )
+        self.page.refresh_from_db()
+        self.assertEqual(len(self.page.related_pages), 2)
+
+    def test_non_live_pages(self):
+        """
+        Management command should not affect pages that are not live
+        """
+        non_live_page = ContentPage(title="Non-live page", slug="non-live-page")
+        home_page = HomePage.objects.first()
+        home_page.add_child(instance=non_live_page)
+        non_live_page.unpublish()
+
+        out = StringIO()
+        call_command("change_related_tag_to_related_page", "--no-dry-run", stdout=out)
+
+        self.assertNotIn(str(non_live_page.id), out.getvalue())
+        self.assertEqual(len(non_live_page.related_pages), 0)
