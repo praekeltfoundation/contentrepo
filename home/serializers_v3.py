622da31322d04a8777adae24354a26378451d415
Fritz Brand <fritz@reachdigitalhealth.org>
2025-07-15 08:04:02 +0200

    Add exception for blank slugs

--- home/serializers_v3.py
+++ home/serializers_v3.py
@@ -209,7 +209,12 @@ def format_whatsapp_body_V3(content_page):
 
 def format_detail_url(obj, request):
     router = request.wagtailapi_router
-    detail_url = get_object_detail_url(router, request, type(obj), obj.id)
+    if not obj.slug:
+        # TODO: Add test for pages and templates
+        raise Exception(
+            f"Error finding detail URL. Blank slug detected for {type(obj)} id={obj.id} {obj}"
+        )
+    detail_url = get_object_detail_url(router, request, type(obj), obj.slug)
     query_params = request.query_params
     if query_params:
         detail_url = (
